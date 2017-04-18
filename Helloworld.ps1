fa49ba7c25473b8d8bad54c9226e4986adc85981
AndyFul <andrzejula@o2.pl>
2017-04-18 23:34:52 +0200

    Add files via upload

---
+++ Helloworld.ps1
@@ -0,0 +1,19 @@
+# Filename: Hello.ps1
+Write-Host
+Write-Host 'Hello World!'
+Write-Host "Good-bye World! `n"
+Write-Host 'Hello World!'
+Write-Host "Good-bye World! `n"
+Write-Host 'Hello World!'
+Write-Host "Good-bye World! `n"
+Write-Host 'Hello World!'
+Write-Host "Good-bye World! `n"
+Write-Host 'Hello World!'
+Write-Host "Good-bye World! `n"
+Write-Host 'Hello World!'
+Write-Host "Good-bye World! `n"
+Write-Host 'Hello World!'
+Write-Host "Good-bye World! `n"
+Write-Host "Press any key to continue ..."
+$x = $host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
+# end of script
