9a87dbf514e915fa115106c79fbcd32067b98e00
AndyFul <andrzejula@o2.pl>
2017-08-01 10:03:08 +0200

    Update Helloworld.ps1

--- Helloworld.ps1
+++ Helloworld.ps1
@@ -1,19 +1,19 @@
 # Filename: Hello.ps1
 Write-Host
 Write-Host 'Hello World!'
-Write-Host "Good-bye World! `n"
+Write-Host "Good-bye World!"
 Write-Host 'Hello World!'
-Write-Host "Good-bye World! `n"
+Write-Host "Good-bye World!"
 Write-Host 'Hello World!'
-Write-Host "Good-bye World! `n"
+Write-Host "Good-bye World!"
 Write-Host 'Hello World!'
-Write-Host "Good-bye World! `n"
+Write-Host "Good-bye World!"
 Write-Host 'Hello World!'
-Write-Host "Good-bye World! `n"
+Write-Host "Good-bye World!"
 Write-Host 'Hello World!'
-Write-Host "Good-bye World! `n"
+Write-Host "Good-bye World!"
 Write-Host 'Hello World!'
-Write-Host "Good-bye World! `n"
+Write-Host "Good-bye World!"
 Write-Host "Press any key to continue ..."
 $x = $host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
 # end of script
